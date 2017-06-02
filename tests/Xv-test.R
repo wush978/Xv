217438afa760b260d68a7e1b0ed134074cade5c1
Wush Wu <wush978@gmail.com>
2017-06-02 19:55:10 +0800

    add test case of: dgRMatrix

--- tests/Xv-test.R
+++ tests/Xv-test.R
@@ -1,5 +1,7 @@
+
 if (isNamespaceLoaded("Xv")) unloadNamespace("Xv")
 m1 <- Matrix::sparse.model.matrix(~ ., iris)
+m2 <- as(m1, "RsparseMatrix")
 stopifnot(class(m1) == "dgCMatrix")
 set.seed(1)
 x1.1 <- rnorm(ncol(m1))
@@ -25,21 +27,28 @@ testXv <- function(name, m, x1, x2, action) {
 
 # Testing with Matrix
 testXv(sprintf("Matrix-%s-numeric", class(m1)), m1, x1.1, x1.2, function(x, y) (x %*% y)@x)
+testXv(sprintf("Matrix-%s-numeric", class(m2)), m2, x1.1, x1.2, function(x, y) (x %*% y)@x)
 
 # Testing with Xv
 loadNamespace("Xv")
 .sm <- selectMethod("%*%", signature(x = class(m1), y = class(x1.1)))
 stopifnot(.sm@defined@package == c("Matrix", "methods"))
 testXv(sprintf("Xv-%s-numeric", class(m1)), m1, x1.1, x1.2, function(x, y) x %*% y)
+testXv(sprintf("Xv-%s-numeric", class(m2)), m2, x1.1, x1.2, function(x, y) x %*% y)
 
 # Verify results
 result.names <- grep("^Matrix", ls(test.env), value = TRUE)
 for(name in result.names) {
   name.ref <- name
   name.test <- gsub("^Matrix", "Xv", name)
-  stopifnot(name.test %in% ls(test.env))
-  stopifnot(is.numeric(test.env[[name.ref]]))
-  stopifnot(is.numeric(test.env[[name.test]]))
-  stopifnot(test.env[[name.ref]] == test.env[[name.test]])
-  stopifnot(attr(test.env[[name.test]], "t")[3] < attr(test.env[[name.ref]], "t")[3])
+  tryCatch({
+    stopifnot(name.test %in% ls(test.env))
+    stopifnot(is.numeric(test.env[[name.ref]]))
+    stopifnot(is.numeric(test.env[[name.test]]))
+    stopifnot(test.env[[name.ref]] == test.env[[name.test]])
+    stopifnot(attr(test.env[[name.test]], "t")[3] < attr(test.env[[name.ref]], "t")[3])
+  }, error = function(e) {
+    cat(sprintf("name:%s\n", name))
+    stop(conditionMessage(e))
+  })
 }
